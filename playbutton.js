ec0c1e86aaeed54b829f6611527e8f911c5bc017
Daniel Navon <dnavon2557@gmail.com>
2016-05-25 00:11:06 -0400

    added footer with info and modals to go with footer

--- playbutton.js
+++ playbutton.js
@@ -30,7 +30,7 @@ function getTitle(litness) {
         } else if (litness >= 3/8) {
                 return "Low Key";
         } else if (litness >= 1/4) {
-                return "J Chillin'";
+                return "J Chillin";
         } else if (litness >= 0) {
                 return "Not Lit";
         }
@@ -38,14 +38,15 @@ function getTitle(litness) {
 
 function setCurrent() {
         var genres = document.getElementsByTagName("a");
-        console.log(genres);
         Array.prototype.forEach.call(genres, function(genre) {
                  genre.onclick = function () {
                         var active = document.getElementsByClassName("active")[0];
-                        if (active != null && active != undefined) {
-                                active.className = "";
+                        if (this.innerHTML != "About" && this.innerHTML != "Contact") {
+                                if (active != null && active != undefined) {
+                                        active.className = "";
+                                }
+                                this.className += " active";
                         }
-                        this.className += " active";
                 };
         });
                
@@ -100,8 +101,112 @@ function getPlaylist(genre, litness) {
         http.send(null);
 
 }
+
+
+function watchModals() {
+        centerModals();
+        var modal_links = document.getElementsByClassName("modal_link");
+        Array.prototype.forEach.call(modal_links, function (link) {
+                link.onclick = function () {
+                        var target_id = this.dataset.target;
+                        var target = document.getElementById(target_id);
+                        backdrop(target); 
+                        slidein(target);
+                };
+        });
+}
+
+function backdrop(target) {
+        var cover = document.getElementById("cover");
+        cover.style.display = "block";
+        fadein(cover);
+        cover.onclick = function() {
+                fadeout(cover);
+                slideout(target);
+        }
+}
+
+function fadeout(cover) {
+        var opacity = 0.6;
+        opacity = parseFloat(cover.style.opacity);
+        opacity = opacity - 0.015;
+        cover.style.opacity = opacity;
+        if (opacity > 0.000) {
+                window.requestAnimationFrame(function () {
+                        fadeout(cover);
+                });
+        }
+        else {
+                        cover.style.display= "none";
+        }
+}
+function slideout(modal) {
+        var top = 5;
+        if (modal.style.top != "") {
+                top = parseFloat(modal.style.top.slice(0, -1));
+        }
+        if (top > -10) {
+                top -= 1.0;
+        }
+        if (top > -25) {
+                top -= 2.0;
+        }
+        if (top > -60) {
+                top -= 2.0;
+                modal.style.top = top.toString() + "%";
+                window.requestAnimationFrame(function () {
+                        slideout(modal);
+                });
+        } 
+}
+
+function centerModals() {
+        var modals = document.getElementsByClassName("modal");
+        Array.prototype.forEach.call(modals, function (modal) {
+                var mwidth = modal.getBoundingClientRect().width;
+                var wwidth = window.innerWidth;
+                modal.style.left = (wwidth/2-mwidth/2).toString() + "px";
+        });
+}
+
+
+function slidein(modal) {
+        var top = -60;
+        if (modal.style.top != "") {
+                top = parseFloat(modal.style.top.slice(0, -1));
+        }
+        if (top < -10) {
+                top += 2;
+        }
+        if (top < -25) {
+                top += 2;
+        }
+        if (top < 5) {
+                top += 2.0;
+                modal.style.top = top.toString() + "%";
+                window.requestAnimationFrame(function () {
+                        slidein(modal);
+                });
+        }
+}
+
+function fadein(modal) {
+        var opacity = 0;
+        if (modal.style.opacity != "") {
+                opacity = parseFloat(modal.style.opacity);
+        }
+        opacity = opacity + 0.025;
+        modal.style.opacity = opacity;
+        if (opacity < 0.6) {
+                window.requestAnimationFrame(function () {
+                        fadein(modal);
+                });
+        }
+}
+
 window.onload = function () {
         setCurrent();
+        watchModals();
 };
 function turnup() {
         var genre = document.getElementsByClassName("active")[0].innerHTML;
